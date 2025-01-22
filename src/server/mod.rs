b9cf8874ff8da6379fc8058e63cb04dd83b0d59f
Dmitry Osipov <dmith.osipov@gmail.com>
2025-01-22 15:06:45 +0700

    update listen addr

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -178,7 +178,7 @@ pub async fn run_server(pool: Arc<Pool>) {
         .with_state(pool)
         .merge(SwaggerUi::new("/swagger").url("/api-docs/openapi.json", ApiDoc::openapi()));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
 
